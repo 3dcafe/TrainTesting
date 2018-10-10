da5573deba9ec98f406dda94078c5d1b84ebbd12
DESKTOP-NDMLNPN\gs3d <gs3d@yandex.ru>
2018-10-10 12:14:07 +0300

    Add project files.

---
+++ TestTrain/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTrain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            this.Text = String.Format("My Application Version {0}", version);
+        }
+
+        private void addRequestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
